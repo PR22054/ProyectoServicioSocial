15ea7338b62184d1112e77ec6499934153de09a0
PR22054 <pr22054@ues.edu.sv>
2026-05-11 21:39:20 -0600

    se crearon las migraciones y modelos para users y anios.

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
 
-//ruta por defecto
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Auth
+Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('login', [LoginController::class, 'login']);
+Route::post('logout', [LoginController::class, 'logout'])->name('logout');
+
+Route::get('/home', function () {
+    return view('home');
+})->middleware('auth')->name('home');

8bc57f6a179db52033e7d4972a73a85dc5fa192e
PR22054 <pr22054@ues.edu.sv>
2026-06-02 22:48:35 -0600

    Se hicieron cambios en los archivos de constancias, se cambio la fecha, se justificaron los textos, se alineo los datos numericos, y por ultimo se agrego la firma y el sello correspondiente

--- public/constancias/Cod_01-60-80-81.blade.php
+++ public/constancias/Cod_01-60-80-81.blade.php
@@ -3,13 +3,13 @@
 <head>
 <meta charset="utf-8">
 <style>
-  body       { font-family: 'DejaVu Sans', sans-serif; font-size: 11px; }
+  body       { font-family: 'DejaVu Sans', sans-serif; font-size: 11px; color: #2B5880; }
+  p          { text-align: justify; }
   .ht        { width: 100%; border-collapse: collapse; margin-bottom: 18px; }
   .ht td     { border: 1px solid #000; padding: 5px; vertical-align: middle; }
   .titulo    { text-align: center; font-weight: bold; margin: 4px 0; }
   .bloque    { margin: 8px 0; }
   .label     { font-weight: bold; }
-  .contenido { color: #2B5880; }
 </style>
 </head>
 <body>
@@ -36,54 +36,56 @@
     </tr>
   </table>
 
-  <div class="contenido">
+  <p class="titulo">ALCALDIA MUNICIPAL DE SANTA ANA NORTE</p>
+  <p class="titulo">NIT: 0214-010524-101-5</p>
+  <br>
+  <p>LA INFRASCRITA AGENTE DE RETENCION EN CUMPLIMIENTO DEL ART. 145 DEL CODIGO TRIBUTARIO HACE CONSTAR QUE:</p>
+  <br>
 
-    <p class="titulo">ALCALDIA MUNICIPAL DE SANTA ANA NORTE</p>
-    <p class="titulo">NIT: 0214-010524-101-5</p>
-    <br>
-    <p>LA INFRASCRITA AGENTE DE RETENCION EN CUMPLIMIENTO DEL ART. 145 DEL CODIGO TRIBUTARIO HACE CONSTAR QUE:</p>
-    <br>
-    <p class="bloque"><span class="label">NOMBRE</span> &nbsp;&nbsp;&nbsp; {{ $nombre }}</p>
-    <p class="bloque"><span class="label">NIT/DUI</span> &nbsp;&nbsp;&nbsp; {{ $nit }}</p>
-    <p class="bloque"><span class="label">CODIGO DE RETENCIÓN</span> &nbsp;&nbsp;&nbsp; {{ $codigo }}</p>
-    <br>
-    <p>DEVENGO LOS SIGUIENTES INGRESOS EN EL AÑO {{ $anio }}:</p>
-    <br>
-    <p class="label">INGRESOS:</p>
-    <p>MONTO DEVENGADO &nbsp;……………………………. $ {{ number_format($monto_devengado, 2) }}</p>
-    <p>AGUINALDO GRAVADO &nbsp;………………................... $ {{ number_format($aguinaldo_gravado, 2) }}</p>
-    <p>AGUINALDO EXENTO &nbsp;………………................... $ {{ number_format($aguinaldo_exento, 2) }}</p>
-    <br>
-    <p class="label">DEDUCCIONES:</p>
-    <p>I.S.S.S. &nbsp;………………................... $ {{ number_format($isss, 2) }}</p>
-    <p>A.F.P. &nbsp;………………................... $ {{ number_format($afp, 2) }}</p>
-    <p>I.P.S.F.A. &nbsp;………………................... $ {{ number_format($ipsfa, 2) }}</p>
-    <p>I.N.P.E.P. / ISSS IVM &nbsp;………………................... $ {{ number_format($inpep, 2) }}</p>
-    <br>
-    <p class="label">IMPUESTO RETENIDO: &nbsp;………………................... $ <u>{{ number_format($impuesto_retenido, 2) }}</u></p>
-    <br>
-    <p>Y PARA LOS EFECTOS DE SU DECLARACION DE IMPUESTO SOBRE LA RENTA, SE EXTIENDE LA PRESENTE EN LA UNIDAD DE TESORERIA DE LA ALCALDIA MUNICIPAL DE SANTA ANA NORTE, DISTRITO DE METAPAN A LOS {{ $dia }} DIAS DEL MES DE {{ $mes }} DE {{ $anio_emision }}.</p>
+  {{-- datos del empleado: dos puntos tras la etiqueta, valor en negrita a la par --}}
+  <p class="bloque"><span class="label">NOMBRE:</span>&nbsp;&nbsp;<span class="label">{{ $nombre }}</span></p>
+  <p class="bloque"><span class="label">NIT/DUI:</span>&nbsp;&nbsp;<span class="label">{{ $nit }}</span></p>
+  <p class="bloque"><span class="label">CODIGO DE RETENCION:</span>&nbsp;&nbsp;<span class="label">{{ $codigo }}</span></p>
 
-    {{-- firma y sello lado a lado, nombre y cargo debajo de ambos --}}
-    <table style="width:100%; margin-top:50px; border-collapse:collapse;">
-      <tr>
-        <td style="width:50%; text-align:center; vertical-align:bottom; padding:4px;">
-          <img src="{{ public_path('images/Firma.png') }}" style="max-height:80px; max-width:160px;">
-        </td>
-        <td style="width:50%; text-align:center; vertical-align:bottom; padding:4px;">
-          <img src="{{ public_path('images/Sello.png') }}" style="max-height:100px; max-width:100px;">
-        </td>
-      </tr>
-      <tr>
-        <td colspan="2" style="text-align:center; padding-top:6px;">
-        <br>  
+  <br>
+  <p>DEVENGO LOS SIGUIENTES INGRESOS EN EL AÑO {{ $anio }}:</p>
+  <br>
+
+  {{-- montos con exactamente 3 puntos entre etiqueta y valor --}}
+  <p class="label">INGRESOS:</p>
+  <p>MONTO DEVENGADO .........................................................${{ number_format($monto_devengado, 2) }}</p>
+  <p>AGUINALDO GRAVADO .......................................................${{ number_format($aguinaldo_gravado, 2) }}</p>
+  <p>AGUINALDO EXENTO ..........................................................${{ number_format($aguinaldo_exento, 2) }}</p>
+  <br>
+  <p class="label">DEDUCCIONES:</p>
+  <p>I.S.S.S. ...............................................................................${{ number_format($isss, 2) }}</p>
+  <p>A.F.P. ..................................................................................${{ number_format($afp, 2) }}</p>
+  <p>I.P.S.F.A. .............................................................................${{ number_format($ipsfa, 2) }}</p>
+  <p>I.N.P.E.P. / ISSS IVM ............................................................${{ number_format($inpep, 2) }}</p>
+  <br>
+  <p class="label">IMPUESTO RETENIDO .............................................<u>${{ number_format($impuesto_retenido, 2) }}</u></p>
+  <br>
+
+  <p>Y PARA LOS EFECTOS DE SU DECLARACION DE IMPUESTO SOBRE LA RENTA, SE EXTIENDE LA PRESENTE EN LA UNIDAD DE TESORERIA DE LA ALCALDIA MUNICIPAL DE SANTA ANA NORTE, DISTRITO DE METAPAN A LOS 19 DIAS DEL MES DE FEBRERO DE 2026.</p>
+
+  {{-- firma centrada en la pagina, sello a la par hacia la derecha --}}
+  <table style="width:100%; margin-top:50px; border-collapse:collapse;">
+    <tr>
+      <td style="width:30%;"></td>
+      <td style="width:35%; text-align:center; vertical-align:bottom; padding:4px;">
+        <img src="{{ public_path('images/Firma.png') }}" style="max-height:100px; max-width:210px;">
+      </td>
+      <td style="width:35%; text-align:left; vertical-align:bottom; padding:4px 4px 4px 24px;">
+        <img src="{{ public_path('images/Sello.png') }}" style="max-height:120px; max-width:120px;">
+      </td>
+    </tr>
+    <tr>
+      <td colspan="3" style="text-align:center; padding-top:6px;">
         <strong>LIC. DELMY MARILIN MURILLOS JERONIMO</strong><br>
-          <strong>TESORERA MUNICIPAL</strong>
-        </td>
-      </tr>
-    </table>
-
-  </div>
+        <strong>TESORERA MUNICIPAL</strong>
+      </td>
+    </tr>
+  </table>
 
 </body>
 </html>
